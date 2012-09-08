0c07e9e7c44f57e937e33c6e1fba8aba2fb54600
pramodv-nyros <pramodv_nyros@yahoo.com>
2012-09-08 15:06:44 +0530

    javascript

---
+++ README.md
@@ -0,0 +1,4 @@
+Generic-Form-Validations-using-JavaScript
+=========================================
+
+The following application consists of generic form validations using javascript
---
+++ form2/JS/validation.js
@@ -0,0 +1,108 @@
+
+//global variables
+	i=0;j=0;
+	radio_list = [];
+	checkbox_list = [];
+	
+//function to validate textboxes
+function validation_for_textbox(element){
+	if(element.value.length == 0){
+		alert(" " +element.name+"  not be empty" );
+		element.focus();
+		return false;
+	}else if(element.value.length >=10 || element.value.length <=2){
+		alert(" " +element.name+" field length be between 3 to 10" );
+		element.focus();
+		return false;
+	}else{
+		return true;
+	}
+}
+//function to validate radio buttons
+function validation_for_radio_buttons(element){
+	var temp = i;
+	temp--;
+	if(element.checked !=true && element.name ==""){
+		alert("please select"+element.value);
+		return false;
+	}
+	if(element.checked == true){
+		return true;
+	}else{
+		while(temp !=-1 ){
+			if(element.name == radio_list[temp].name){
+				if((element.checked == true)|| (radio_list[temp].checked == true))
+					return true;
+			}
+			temp--;
+		}
+	}
+	alert("please select "+element.name);
+	return false;
+}
+//function to validate select boxes
+function validation_for_select_boxes(element){
+	if(element.value != "Default")
+		return true;
+	else{
+		alert("please select "+ element.name);
+		return false;
+	}
+}
+//function to validate checkboxes		
+function validation_for_checkboxes(element){
+	
+	var temp = j;
+	var flag = 0;
+	temp--;
+	if(element.checked !=true && element.name ==""){
+		alert("please select"+element.value);
+		return false;
+	}
+	
+	if(element.checked == true){
+		return true;
+	}else{
+		while(temp !=-1 ){
+			if(element.name == checkbox_list[temp].name){
+				if((element.checked == false)&& (checkbox_list[temp].checked == false)){
+					flag++;
+					
+				}
+			}
+			temp--;
+		}
+	}
+	if(flag == j){
+		alert("please select "+element.name);
+		return false;
+	}else{
+		return true;
+	}
+}
+//to get a number of checkbox elements in globally declared array
+function toGetCheckbox_list(root){
+	//var root = document.registration.elements;
+	var elementNumber;
+	for(elementNumber = 0;elementNumber<document.registration.elements.length;elementNumber++){
+		if(root[elementNumber].type == "checkbox"){
+			if(root[elementNumber].name != ""){
+				checkbox_list[j]=root[elementNumber];
+				j++;
+			}
+		}
+	}
+}
+//to get a number of checkbox elements in globally declared array
+function toGetRadio_list(root){
+	//var root = document.registration.elements;
+	var elementNumber;
+	for(elementNumber = 0;elementNumber<document.registration.elements.length;elementNumber++){
+		if(root[elementNumber].type == "radio"){
+			if(root[elementNumber].name != ""){
+				radio_list[i]=root[elementNumber];
+				i++;
+			}
+		}
+	}
+}
